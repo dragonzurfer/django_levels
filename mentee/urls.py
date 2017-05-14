0847848271185ad54e16e59e0195a7c2d0856c3b
dragonzurfer <dragonzurfer@gmail.com>
2017-05-14 17:06:09 +0530

    Add link for rating update

--- mentee/urls.py
+++ mentee/urls.py
@@ -9,4 +9,5 @@ urlpatterns=[
 	url(r'^rate$', views.Submission_view.as_view(), name='submission'),
 	url(r'^index$', views.SubmissionIndex.as_view(), name='submission_index'),
 	url(r'^create$',views.CreateMentee.as_view(),name='create_mentee'),
+	url(r'^uPdAtE$', views.EloRating.as_view(), name='elo_rating'),
 ]
