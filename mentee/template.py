95653ff7e6a47516067511b7cccfa7ac574a54f1
dragonzurfer <dragonzurfer@gmail.com>
2017-05-14 17:19:09 +0530

    add template for rating_index.html

--- mentee/template.py
+++ mentee/template.py
@@ -1,3 +1,4 @@
 submission="mentee/submission.html"
 submission_index_template='mentee/submission_index.html'
 create_mentee='mentee/create_mentee.html'
+rating_index_template='mentee/rating_index.html'
